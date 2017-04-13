752469851fba5c4e4ba3b4109aa623783ade7678
Yubo Zhou <yubo@cse.yorku.ca>
2017-04-13 00:45:08 -0700

    lots bugs, fixed all bugs

--- sort/quicksort.py
+++ sort/quicksort.py
@@ -1,4 +1,6 @@
-def quick_sort(li, l, r):
+def quick_sort(li, l, r, verbose=False):
+    if verbose:
+        print('init:',l,r, li)
     # print(li)
     if l >= r:
         return
@@ -6,30 +8,42 @@ def quick_sort(li, l, r):
     head = l
     tail = r
     pivot  = l
-    l += 1
+    # l += 1
+    if verbose:
+        print('summary', head,tail,pivot,l,r)
     # move the pointers
     while l < r:
-        if li[l] < li[pivot]:
-            l += 1
-        if li[r] >= li[pivot]:
-            r -= 1
+        if verbose:
+            print(pivot,l,r,end=' ')
         if li[r] < li[pivot] < li[l]:
             tmp = li[l]
             li[l] = li[r]
             li[r] = tmp
             # i'm moving this pointer again! don't do it!
-            # l += 1
-            # r -= 1
-    # swap the value of pivot and l
-    tmp = li[l]
-    li[l] = li[pivot]
-    li[pivot] = tmp
-    pivot = l
-    print(li)
-    quick_sort(li, head, pivot-1)
-    quick_sort(li, pivot, tail)
+            l += 1
+            r -= 1
+        if li[l] <= li[pivot]:
+            l += 1
+        if li[r] >= li[pivot]:
+            r -= 1
+        if verbose:
+            print(l,r)
+        # print(li)
+    # swap the value of pivot IN VALID case
+    if li[pivot] > li[r]:
+        tmp = li[r]
+        li[r] = li[pivot]
+        li[pivot] = tmp
+        # only increment after swap
+        pivot = r
+    if verbose:
+        print(li)
+    quick_sort(li, head, pivot-1, verbose=verbose)
+    # i forgot to increment right pointer. Once we sorted current pivot, leave current pivot untouched and sort rest of array
+    quick_sort(li, pivot+1, tail, verbose=verbose)
 
 # there's a bug
-l1 = [2,1,5,3,7]
-quick_sort(l1, 0, 2)
+l1 = [3,3,1,3]
+print(l1)
+quick_sort(l1, 0, len(l1)-1, verbose=False)
 print(l1)
