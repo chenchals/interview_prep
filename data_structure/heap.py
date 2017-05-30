8ca7538b7f0f271b21dd645696b4399e7bf651a3
Yubo Zhou <yubo@cse.yorku.ca>
2017-05-29 21:50:47 -0700

    fucking link list

---
+++ data_structure/heap.py
@@ -0,0 +1,33 @@
+class Heap(object):
+    def __init__(self):
+        '''
+        Max/min heap with array implementation
+        '''
+
+    def set(self, x):
+        '''
+        Add if x not in cache
+        :param x:
+        :return:
+        '''
+
+    def get(self, x):
+        '''
+        return None if x doesn't exist cache
+        :param x:
+        :return:
+        '''
+
+    def top(self):
+        '''
+        return Max of Min of the heap
+        :return:
+        '''
+
+    def delete(self, x):
+        '''
+        delete x, return False if x doesn't exist in cache
+        http://www.mathcs.emory.edu/~cheung/Courses/171/Syllabus/9-BinTree/heap-delete.html
+        :param x:
+        :return:
+        '''
